95c2ac23158a396e5cdf1b797e957d2107263e79
Aberreth <aberreth@csi.edu>
2026-09-21 11:02:24 -0600

    Submission for lab 6

--- week6/lab.sql
+++ week6/lab.sql
@@ -1,20 +1,20 @@
--- Creating the table.
+-- Part A: Importing data
+CREATE TABLE product(id integer PRIMARY KEY, product_name varchar(20), brand varchar(20), category text, price numeric(10,2), stock integer);
 
-CREATE TABLE products_raw(id integer PRIMARY KEY, product_name varchar(20), brand varchar(20), category text, price numeric(10,2), stock integer);
-
-SELECT * FROM public.products_raw LIMIT 5;
+SELECT * FROM public.product LIMIT 5;
 -- Steps to import:
 -- Right click on the table > import/export > import, make sure format is correct, header is selected, encoding is UTF8.
 
--- Built in functions of Postgre
+-- Part B – Math & Stats Queries
 -- SUM
 select SUM(price*stock)as total_inventory_value
-from products_raw
+from product
 
 -- AVG
-select AVG(price) as av_price from products_raw
+select AVG(price) as av_price from product
 
-select MIN(price), MAX(price) from products_raw
+-- Highest and lowest price
+select MIN(price), MAX(price) from product
 
 -- COUNT
-select COUNT(*) from products_raw
+select COUNT(*) from product
